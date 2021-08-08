a6cc6a70647b503d274ccb942895bf19221eef22
goryachev <kvant87@gmail.com>
2021-08-08 22:29:25 +0300

    merged function_pool and executor classes

--- executor.cpp
+++ executor.cpp
@@ -2,63 +2,101 @@
 
 void Executor::init()
 {
-  for (int i = 0; i < num_threads; i++)
-  {
-    thread_pool.push_back(std::thread(&Function_pool::infinite_loop_func, &func_pool));
-  }
-  std::cout << "number of threads = " << num_threads << std::endl;
+    std::cout << "number of threads = " << num_threads << std::endl;
+    for (int i = 0; i < num_threads; i++)
+    {
+        promises.push_back(std::promise<void>());
+        futures.push_back(promises.back().get_future());
+        thread_pool.push_back(std::thread(&Executor::infinite_loop_func, this));
+    }
 }
 
-Executor::Executor()
+Executor::Executor() : m_function_queue(), m_lock(), m_data_condition(), m_accept_functions(true)
 {
-  num_threads = std::thread::hardware_concurrency();
-  init();
+    num_threads = std::thread::hardware_concurrency();
+    init();
 };
 
-Executor::Executor(const int64_t N)
+Executor::Executor(const int64_t N) : m_function_queue(), m_lock(), m_data_condition(), m_accept_functions(true)
 { 
-  num_threads = N;
-  init();
+    num_threads = N;
+    init();
 };
 
 void Executor::push_func(std::function<void()> func)
 {
-  num_functions++; 
-  func_pool.push(func);
+    num_functions++; 
+    std::unique_lock<std::mutex> lock(m_lock);
+    m_function_queue.push(func);
+    // when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+    lock.unlock();
+    m_data_condition.notify_one();
+}
+
+void Executor::done()
+{
+    std::unique_lock<std::mutex> lock(m_lock);
+    m_accept_functions = false;
+    lock.unlock();
+    // when we send the notification immediately, the consumer will try to get the lock , so unlock asap
+    m_data_condition.notify_all();
+    //notify all waiting threads.
+}
+
+void Executor::infinite_loop_func()
+{
+    std::function<void()> func;
+    while (true)
+    {
+        {
+            std::unique_lock<std::mutex> lock(m_lock);
+            m_data_condition.wait(lock, [this]() {return !m_function_queue.empty() || !m_accept_functions; });
+            if (!m_accept_functions && m_function_queue.empty())
+            {
+                //lock will be release automatically.
+                //finish the thread loop and let it join in the main thread.
+                return;
+            }
+            func = m_function_queue.front();
+            m_function_queue.pop();
+            //release the lock
+        }
+        func();
+    }
 }
 
 void Executor::synchronize() 
 {
-    func_pool.sync();
+    for (const auto& future : futures) future.wait();
     std::cout << "Synchronization\n";
 }
 
 void Executor::finalize()
 {
-  func_pool.done();
-  for (unsigned int i = 0; i < thread_pool.size(); i++)
-  {
+    done();
+    for (unsigned int i = 0; i < thread_pool.size(); i++)
+    {
       thread_pool.at(i).join();
-  }
-  isFinalized = true;
-  std::cout << "number of executed functions = " << num_functions << std::endl;
+    }
+    isFinalized = true;
+    std::cout << "number of executed functions = " << num_functions << std::endl;
 };
 
 int64_t Executor::get_num_threads()
 {
-   return thread_pool.size();
+    return thread_pool.size();
 }
 
 int64_t Executor::get_num_threads() const
 {
-   return thread_pool.size();
+    return thread_pool.size();
 }
 
 Executor::~Executor()
 {
-   if(!isFinalized)
-   { 
-      finalize();
-   }
-   thread_pool.clear();
+    if(!isFinalized)
+    { 
+        finalize();
+    }
+    thread_pool.clear();
 }
--- executor.h
+++ executor.h
@@ -1,24 +1,43 @@
 #pragma once
 #include <string>
 #include <iostream>
-#include <mutex>
-#include <functional>
 #include <thread>
 #include <vector>
-#include "function_pool.h"
+#include <queue>
+#include <functional>
+#include <future>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <cassert>
+
+
+/**
+*  Chapter 3 "C++ Concurrency in Action", A. Williams
+*/
 
 class Executor
 {
 private:
-    Function_pool func_pool;
+    std::queue<std::function<void()>> m_function_queue;
+    std::vector<std::thread> thread_pool;
+    std::vector<std::promise<void>> promises;
+    std::vector<std::future<void>> futures;
+
+    std::mutex m_lock;
+    std::condition_variable m_data_condition;
+    std::atomic<bool> m_accept_functions;
+ 
     int64_t num_threads=0;
     int64_t num_functions=0; 
-    std::vector<std::thread> thread_pool;
+
     void init();
     bool isFinalized=false;
 public:
     Executor();
     Executor(const int64_t N);
+    void done();
+    void infinite_loop_func();
     void push_func(std::function<void()> func);
     void synchronize();
     void finalize();
